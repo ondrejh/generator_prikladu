1481df0b931dc29e62e48da562e35cfc8e6f6dd4
ondrej <ondrejh.ck@gmail.com>
2013-04-12 12:51:58 +0200

    uprava pro kompatibilitu s matlabem a trocha vzhledu (vetsi font, jen dva sloupce ...)

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ generator_prikladu
 EN: Simple math exercise generator for the first class students.
 
 Generator pocetnich prikladu pro prvnaky.
-Napsano v GNU/Octave - nejspis pujde konvertovat do matlabu.
+Napsano v GNU/Octave. Upraveno pro kompatibilitu s matlabem.
+Popis funkce:
+	Po spusteni vygeneruje skript urceny pocet prikladu na scitani a odcitani
+	a nasledne vygeneruje html tabulku pro tisk. Nastavene hodnoty odpovidaji
+	plne obsazene strance prikladu ve dvou sloupcich. Pro spravne zobrazeni
+	je treba mit ve stejnem adresari stazeny soubor style.css a pro vystup
+	v prohlizeci i ikonku calculator.ico.
 
 ToDo:
 1) Prepsat do nejakeho rozsirenejsiho jazyka s moznosti vygenerovat binarku.
--- gener.m
+++ gener.m
@@ -3,13 +3,13 @@ close all
 clear all
 
 jmeno_souboru = 'priklady.html'; %nazev souboru s vystupem
-celkem_prikladu = 60; %celkovy pocet prikladu (s danym stylem odpovida 1 strance)
-prikladu_na_radek = 3; %nemenit! (muselo by se to spravit ve stylu)
+celkem_prikladu = int32(34); %celkovy pocet prikladu (s danym stylem odpovida 1 strance)
+prikladu_na_radek = int32(2); %nemenit! (muselo by se to spravit ve stylu)
 
 %zarovnani poctu prikladu
-if idivide(celkem_prikladu,prikladu_na_radek)*prikladu_na_radek!=celkem_prikladu
+if idivide(celkem_prikladu,prikladu_na_radek)*prikladu_na_radek~=celkem_prikladu
 	celkem_prikladu = idivide(celkem_prikladu,prikladu_na_radek,'ceil')*prikladu_na_radek;
-endif
+end %if
 
 priklady = [];
 pocet = 0;
@@ -23,60 +23,78 @@ while (pocet<celkem_prikladu)
 	v = 1; %platnost
 
 	%overeni platnosti prikladu
-	if ((z==1)&&(b>=a)) v=0 endif %pri odecitani nesmi byt b>=a
-	if ((z==0)&&((a+b)>20)) v=0 endif %soucet nesmi byt vic nez 20
-	if ((z==1)&&(b>10)) v=0 endif %pri odecitani nesmi byt b>10
-	if ((a==0)||(b==0)) v=0 endif %a nebo b nesmi byt 0
+	if ((z==1)&&(b>=a))
+		v = 0;
+	end %pri odecitani nesmi byt b>=a
+	if ((z==0)&&((a+b)>20))
+		v = 0;
+	end %soucet nesmi byt vic nez 20
+	if ((z==1)&&(b>10))
+		v = 0; 
+	end %pri odecitani nesmi byt b>10
+	if ((a==0)||(b==0)) 
+		v = 0; 
+	end %a nebo b nesmi byt 0
 
 	%pridani do seznamu
 	if (v==1)
 		%test jestli se priklad neopakuje
 		p = 0;
 		for i=1:pocet
-			if ((a==priklady(i,1))&&(z==priklady(i,2))&&(b==priklady(i,3))) p=1 endif
-		endfor
+			if ((a==priklady(i,1))&&(z==priklady(i,2))&&(b==priklady(i,3))) 
+				p=1;
+			end %if
+		end %for
+
 		%pridani prikladu na konec seznamu
 		if (p==0)
 			priklady(pocet+1,:)=[a,z,b];
-			pocet++;
-		endif
-	endif
+			pocet=pocet+1;
+		end %if
+	end %if
 
-endwhile
+end %while
+
 
 %zobrazeni prikladu
 pocet
 priklady
 
 %vygenerovani vystupu
-fid = fopen (jmeno_souboru, "w");
+fid = fopen (jmeno_souboru, 'w');
 
 %hlavicka
-fwrite(fid,["<!DOCTYPE HTML>\n"\
-			"<html>\n"\
-			"<head>\n"\
-			"   <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n"\
-			"   <title>Generator prikladu</title>\n"\
-			"   <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" media=\"screen\" />\n"\
-			"   <link rel=\"stylesheet\" href=\"style.css\" media=\"print\" />\n"\
-			"   <link rel=\"shortcut icon\" href=\"calculator.ico\" />\n"\
-			"</head>\n"\
-			"<body>\n"\
-			"<section id=main>\n"\
-			"<article><p><table>\n"]);
-			
+fprintf(fid,['<!DOCTYPE HTML>\n' ...
+			 '<html>\n' ...
+			 '<head>\n' ...
+			 '<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n' ...
+			 '<title>Generator prikladu</title>\n' ...
+			 '<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" media=\"screen\" />\n'...
+			 '<link rel=\"stylesheet\" href=\"style.css\" media=\"print\" />\n' ...
+			 '<link rel=\"shortcut icon\" href=\"calculator.ico\" />\n' ...
+			 '</head>\n' ...
+			 '<body>\n' ...
+			 '<section id=main>\n' ...
+			 '<article><p><table>\n']);
+
 %priklady
 for i=1:pocet
-	if (i-idivide(i,prikladu_na_radek)*prikladu_na_radek==1) fwrite(fid,"   <tr>"); endif
+	if (i-idivide(i,prikladu_na_radek)*prikladu_na_radek==1) 
+		fprintf(fid,' <tr>'); 
+	end %if
 	a=priklady(i,1);
 	z='+';
-	if priklady(i,2)==1 z='-'; endif
+	if priklady(i,2)==1 
+		z='-';
+	end %if
 	b=priklady(i,3);
-	fprintf(fid,"<td>%d</td><td>%c</td><td>%d</td><td>=</td>&nbsp;<td></td>",a,z,b);
-	if (i-idivide(i,prikladu_na_radek)*prikladu_na_radek==0) fwrite(fid,"</tr>\n"); endif
-endfor
+	fprintf(fid,'<td>%d</td><td>%c</td><td>%d</td><td>=</td>&nbsp;<td></td>',a,z,b);
+	if (i-idivide(i,prikladu_na_radek)*prikladu_na_radek==0) 
+		fprintf(fid,'</tr>\n'); 
+	end %if
+end %for
 
 %zapati
-fwrite(fid,"</table></p></article>\n");
+fprintf(fid,'</table></p></article>\n');
 
 fclose (fid);
